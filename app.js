2ff214a05a1d2284a5eae6cda7f4d8ee34f4f2c8
Jérôme CELEBI <jerome.celebi@MacBook-Pro-de-Jerome.local>
2020-02-14 11:35:48 +0100

    Playground

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const app = express();
-
+const expressPlayground = require("graphql-playground-middleware-express")
+  .default;
 const schema = require("./schema/schema");
 
 app.use(
@@ -11,6 +12,8 @@ app.use(
     graphiql: true
   })
 );
+
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 app.listen(4000, () => {
   console.log("app listening on port 4000");
